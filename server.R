8bdfd7855a95d78466a16958177c08dc4ffe0118
benmwhite <bw2121@gmail.com>
2014-11-10 16:25:34 -0600

    edited sidebar

--- server.R
+++ server.R
@@ -3,14 +3,12 @@ library(ggplot2)
 shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     x <- seq(from = -4, to = 4, by = 0.05)
-    y <- dt(x, df)
-    t_a <- qt(1 - alpha, df)
+    y <- dt(x, input$deg_f)
     qplot(x, y, geom = "line", xlim = c(-4, 4),
-          ylim = c(0, 0.6), 
-          main = paste("Student's t Distribution, df =", as.character(df),
-                       ", critical value =", as.character(round(t_a, 4))),
+          ylim = c(0, 0.5), 
+          main = paste("Student's t Distribution, df =", 
+                       as.character(input$deg_f)),
           xlab = "t", ylab = "f(t)") + 
-      geom_vline(xintercept = t_a, linetype = "dotted", color = "blue") + 
-      theme_minimal()
+      theme_minimal() + stat_function(fun = dnorm, color = "blue")
   })
 })
--- ui.R
+++ ui.R
@@ -3,16 +3,12 @@ shinyUI(fluidPage(
   titlePanel("Visualizing Student's t Distribution"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("df",
+      sliderInput("deg_f",
                   "degrees of freedom:",
                   min = 1,
-                  max = 40,
-                  value = 20,
-                  step = 1),
-      radioButtons("alpha",
-                   "significance level (alpha):",
-                   list(0.001, 0.01, 0.05, 0.1),
-                   selected = 0.05)
+                  max = 20,
+                  value = 10,
+                  step = 1)
     ),
     mainPanel(
       plotOutput("distPlot")
