41214653a475503a10d7583c3c3c7d64579bc755
Benjamin White <bw2121@gmail.com>
2018-04-23 20:58:51 -0500

    added significance level slider and tail checkbox, minor ui edits

--- server.R
+++ server.R
@@ -4,11 +4,16 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     x <- seq(from = -4, to = 4, by = 0.05)
     y <- dt(x, input$deg_f)
+    crit <- qt(input$sig, df = input$deg_f, lower.tail = input$tail)
     qplot(x, y, geom = "line", xlim = c(-4, 4),
-          ylim = c(0, 0.5), 
+          ylim = c(0, 0.45), 
           main = paste("Student's t Distribution, df =", 
-                       as.character(input$deg_f)),
+                       as.character(input$deg_f),
+                       "critical value = ",
+                       as.character(round(crit, 4))),
           xlab = "t (blue density is standard normal)", ylab = "f(t)") + 
-      theme_minimal() + stat_function(fun = dnorm, color = "blue")
+      theme_minimal() + 
+      stat_function(fun = dnorm, color = "blue") +
+      geom_vline(xintercept = crit, colour = "red")
   })
 })
--- ui.R
+++ ui.R
@@ -4,11 +4,18 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       sliderInput("deg_f",
-                  "degrees of freedom:",
+                  "Degrees of freedom:",
                   min = 1,
-                  max = 20,
+                  max = 25,
                   value = 10,
-                  step = 1)
+                  step = 1),
+      sliderInput("sig",
+                  "Level of signinficance:",
+                  min = 0.005, 
+                  max = 0.1,
+                  value = 0.05,
+                  step = 0.005),
+      checkboxInput("tail", "Lower tail")
     ),
     mainPanel(
       plotOutput("distPlot")
